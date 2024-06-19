ffb95b07c4fffc52e05a584f8d6926c44861bf09
LuisFerdinand <ferdinandluis88@gmail.com>
2024-06-19 07:50:37 +0700

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,39 @@
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-Menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+// navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+function validateForm() {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+    
+    if (!name || !email || !message) {
+      alert('All fields are required.');
+      return false;
+    }
+    
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+      alert('Please enter a valid email address.');
+      return false;
+    }
+    
+    return true;
+  }
